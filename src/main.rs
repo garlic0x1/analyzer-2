2a3eb01ad704cc37e215e4b51c9aa761983ddef1
garlic0x1 <garlic1@protonmail.com>
2022-06-17 21:53:53 -0700

    built framework for graph

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,54 @@ use std::fs;
 use tree_sitter::{Node, Parser, Tree, TreeCursor};
 use tree_sitter_php;
 
-struct Context {}
+// not same thing as context in last version
+// this is to store hook/html stuff
+struct Context {
+    kind: String,
+    name: String,
+}
 
-struct Taint {}
+// variable scope, same as old context, but with file
+struct Scope {
+    file: String,
+    class: Option<String>,
+    function: Option<String>,
+}
+
+// there should be major and minor vertices
+// major vertices are parts of code where an
+// assignment or function call happens. A minor
+// vertex is an object on the stack to be
+// traced through
+struct Vertex {
+    kind: String,
+
+    // help sonic find the code
+    source_code: String,
+    position: tree_sitter::Point,
+    context: Context,
+
+    // these are the children on the graph
+    // (a child may have more than one parent)
+    arcs: Vec<Arc>,
+}
+
+struct MinorVertex {
+    kind: String,
+    name: String,
+}
+
+struct Arc {
+    stack: Vec<MinorVertex>,
+    // an immutable pointer to a vertex
+    pointer: *const Vertex,
+}
+
+struct Taint {
+    kind: String,
+    name: String,
+    scope: Scope,
+}
 
 struct Analyzer {
     tree: Tree,
@@ -15,7 +60,7 @@ struct Analyzer {
 }
 
 impl Analyzer {
-    fn new(tree: Tree, source_code: String) -> Self {
+    pub fn new(tree: Tree, source_code: String) -> Self {
         Self {
             tree,
             source_code,
@@ -25,7 +70,7 @@ impl Analyzer {
         }
     }
 
-    fn traverse(&mut self) -> Result<(), ()> {
+    pub fn traverse(&mut self) -> Result<(), ()> {
         let t = self.tree.clone();
         let mut cursor = t.walk();
         let mut visited = false;
@@ -51,6 +96,33 @@ impl Analyzer {
         Ok(())
     }
 
+    fn trace_taint(&mut self, cursor: &mut TreeCursor) {
+        while cursor.goto_parent() {
+            match cursor.node().kind() {
+                "function_call_expression" => {}
+                "method_call_expression" => {}
+                "echo_statement" => {}
+                _ => (),
+            }
+        }
+    }
+
+    fn trace_name(&self, cursor: &mut TreeCursor) {
+        while cursor.goto_parent() {
+            match cursor.node().kind() {
+                "function_call_expression" => {
+                    println!("FUNC CALL NAME");
+                    break;
+                }
+                "variable_name" => {
+                    println!("VAR NAME");
+                    break;
+                }
+                _ => (),
+            }
+        }
+    }
+
     // call with a cloned TreeCursor to not lose our place in the traversal
     fn enter_node(&mut self, cursor: &mut TreeCursor, tabs: usize) -> Result<(), ()> {
         let node = cursor.node();
@@ -63,7 +135,7 @@ impl Analyzer {
             );
         }
         match node.kind() {
-            "name" => trace_name(cursor),
+            "name" => self.trace_name(cursor),
             _ => (),
         }
         Ok(())
@@ -81,33 +153,6 @@ fn main() -> Result<(), ()> {
     let mut analyzer = Analyzer::new(tree.clone(), source_code);
     analyzer.traverse()?;
     Ok(())
-}
-
-fn trace_taint(cursor: &mut TreeCursor) {
-    while cursor.goto_parent() {
-        match cursor.node().kind() {
-            "function_call_expression" => {}
-            "method_call_expression" => {}
-            "echo_statement" => {}
-            _ => (),
-        }
-    }
-}
-
-fn trace_name(cursor: &mut TreeCursor) {
-    while cursor.goto_parent() {
-        match cursor.node().kind() {
-            "function_call_expression" => {
-                //println!("FUNC CALL NAME");
-                break;
-            }
-            "variable_name" => {
-                //println!("VAR NAME");
-                break;
-            }
-            _ => (),
-        }
-    }
 }
 
 fn node_to_string(node: &Node, source: &str) -> String {
