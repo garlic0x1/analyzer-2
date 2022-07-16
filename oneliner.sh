1e7b0922b11f042c142141839f18cf052285f821
garlic0x1 <garlic1@protonmail.com>
2022-07-16 15:31:21 -0700

    fixed shell file

--- oneliner.sh
+++ oneliner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-while read line; do echo $line | hakrawler -i -d 3 | grep ".php" | xargs -I %s printf "%s " | tee |  ./target/debug/analyzer ; done;
+while read line; do echo $line | hakrawler -i -d 3 | grep ".php" | xargs -I %s printf "%s " | tee |  ./target/debug/analyzer ; done < trunks.txt
 
