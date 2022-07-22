d89de555b25a1a3b6a9f581722d7ee60e37a134e
garlic0x1 <garlic1@protonmail.com>
2022-07-22 12:07:25 -0700

    iter

--- src/tree/traverser.rs
+++ src/tree/traverser.rs
@@ -4,6 +4,7 @@ pub struct Traversal<'a> {
     cursor: Cursor<'a>,
     visited: bool,
     start: Cursor<'a>,
+    last: bool,
 }
 
 impl<'a> Traversal<'a> {
@@ -12,6 +13,7 @@ impl<'a> Traversal<'a> {
             start: cursor.clone(),
             cursor,
             visited: false,
+            last: false,
         }
     }
 }
@@ -21,29 +23,41 @@ pub enum Order<'a> {
     Leave(Cursor<'a>),
 }
 
+/// preorder and postorder together, for pushing and popping to context stack
 impl<'a> Iterator for Traversal<'a> {
     type Item = Order<'a>;
 
+    /// get the next step in iteration
     fn next(&mut self) -> Option<Self::Item> {
         let last = self.cursor.clone();
 
+        if self.last {
+            return None;
+        }
+
         if self.visited {
+            // break when we have completely visited start
+            if last == self.start {
+                self.last = true;
+            }
+
             if self.cursor.goto_next_sibling() {
                 self.visited = false;
-                // returning enter
+                // leave
                 return Some(Order::Leave(last));
             } else if self.cursor.goto_parent() {
-                // returning leave
+                // leave
                 return Some(Order::Leave(last));
             } else {
-                return None;
+                self.last = true;
+                return Some(Order::Leave(last));
             }
         } else {
             if self.cursor.goto_first_child() {
-                // return enter
+                // enter
                 return Some(Order::Enter(last));
             } else {
-                // leave
+                // enter
                 self.visited = true;
                 return Some(Order::Enter(self.cursor.clone()));
             }
